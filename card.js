adf02361a84ed8d7bb07961498336b36bb03325a
jennyraj <jennyraj@users.noreply.github.com>
2021-05-03 11:28:50 -0400

    Add files via upload
    initial setup

---
+++ card.js
@@ -0,0 +1,39 @@
+
+import React from "react";
+import {Button, Card, CardActions, CardContent, Typography} from "@material-ui/core";
+
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    root: {
+
+    },
+}));
+
+export default function SimpleCard() {
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+
+    return (
+        <Card className={classes.root}>
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    Word of the Day
+                </Typography>
+                <Typography variant="h5" component="h2">
+                    be{bull}nev{bull}o{bull}lent
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                    adjective
+                </Typography>
+                <Typography variant="body2" component="p">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+    )}
